208014a94dbb7496f121c5f0320899156c24082a
dorian <dorian.vidal@hetic.net>
2022-07-08 10:49:35 +0200

    add page courses

---
+++ pages/courses.vue
@@ -0,0 +1,25 @@
+<template>
+  <section id="page-courses" class="">
+    <div class="overflow-hidden">
+      <div class="mx-auto">COURSES</div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Courses",
+
+  data() {
+    return {};
+  },
+  head() {
+    return {
+      bodyAttrs: {
+        class: "courses",
+      },
+    };
+  },
+  methods: {},
+};
+</script>
